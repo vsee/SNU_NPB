68abec1b10ce7534d8bd1c8c4ad6fe42fb54fa0c
Volker Seeker <volker.seeker@ed.ac.uk>
2020-12-11 15:07:14 +0000

    custom clang configured

--- NPB3.3-SER-C/run.sh
+++ NPB3.3-SER-C/run.sh
@@ -3,12 +3,20 @@
 mkdir -p bin
 mkdir -p result
 
+#foreach benchmark ( ft mg cg lu bt is ep sp )
+#    foreach class ( S W A B C D )
+#        echo "compiling $benchmark.$class. (SER-C)"
+#        make $benchmark CLASS=$class
+#        echo "running $benchmark.$class. (SER-C)"
+#        bin/$benchmark.$class.x > result/$benchmark.$class.out
+#        echo "done.\n"
+#    end
+#end
+
 foreach benchmark ( ft mg cg lu bt is ep sp )
-    foreach class ( S W A B C D )
+    foreach class ( S )
         echo "compiling $benchmark.$class. (SER-C)"
         make $benchmark CLASS=$class
-        echo "running $benchmark.$class. (SER-C)"
-        bin/$benchmark.$class.x > result/$benchmark.$class.out
         echo "done.\n"
     end
 end

8658d1f62a9814369b3d73cfcdf8182d72a0f5ea
Volker Seeker <volker.seeker@ed.ac.uk>
2022-03-25 09:15:05 +0000

    workload class W supported for FT

--- NPB3.3-SER-C/FT/auto2_verify.c
+++ NPB3.3-SER-C/FT/auto2_verify.c
@@ -30,7 +30,7 @@ static inline dcomplex dcmplx_div(dcomplex z1, dcomplex z2) {
 // this will not be touched by any optimisations done by auto2 to keep verification and
 // benchmark separated
 //
-// it is only inteded to work for input class S
+// it is only inteded to work for input class S and W
 void auto2_verify(int n1, int n2, int n3, int nt, dcomplex cksum[nt+1]) {
 
     // Local variables.
@@ -43,7 +43,7 @@ void auto2_verify(int n1, int n2, int n3, int nt, dcomplex cksum[nt+1]) {
     logical verified = true;
 
 
-    if ((n1 == 64) && (n2 == 64) && (n3 == 64) && (nt == 6)) { // check if class S
+    if ((n1 == 64) && (n2 == 64) && (n3 == 64) && (nt == 6)) {
         // Class S reference values.
         cexpd[1] = dcmplx(554.6087004964, 484.5363331978);
         cexpd[2] = dcmplx(554.6385409189, 486.5304269511);
@@ -51,6 +51,14 @@ void auto2_verify(int n1, int n2, int n3, int nt, dcomplex cksum[nt+1]) {
         cexpd[4] = dcmplx(554.5423607415, 490.1273169046);
         cexpd[5] = dcmplx(554.4255039624, 491.7475857993);
         cexpd[6] = dcmplx(554.2683411902, 493.2597244941);        
+    } else if ((n1 == 128) && (n2 == 128) && (n3 == 32) && (nt == 6)) {
+        // Class W reference values.
+        cexpd[1] = dcmplx(567.3612178944, 529.3246849175);
+        cexpd[2] = dcmplx(563.1436885271, 528.2149986629);
+        cexpd[3] = dcmplx(559.4024089970, 527.0996558037);
+        cexpd[4] = dcmplx(556.0698047020, 526.0027904925);
+        cexpd[5] = dcmplx(553.0898991250, 524.9400845633);
+        cexpd[6] = dcmplx(550.4159734538, 523.9212247086);
     } else {
         printf("AUTO2: Invalid workload class used for verification.\n");
         return;
